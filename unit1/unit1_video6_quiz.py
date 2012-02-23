6e6235795698490838b6bbd1ec38d2c2945bd955
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-23 23:46:09 +0200

    solution

--- unit1/unit1_video6_quiz.py
+++ unit1/unit1_video6_quiz.py
@@ -1,8 +1,10 @@
-#Modify your code to create probability vectors, p, of arbitrary 
-#size, n. Use n=5 to verify that your new solution matches 
-#the previous one.
+#Modify the empty list, p, so that it becomes a UNIFORM probability
+#distribution over five grid cells, as expressed in a list of 
+#five probabilities.
 
-p=[]
 n=5
-
+world_size = n
+p = []
+for i in range(0,world_size):
+    p.append(1.0/world_size)
 print p
--- unit1/unit1_video7_quiz.py
+++ unit1/unit1_video7_quiz.py
@@ -8,3 +8,8 @@ p = []
 for i in range(0,world_size):
     p.append(1.0/world_size)
 print p
+sense = [0.2, 0.6, 0.6, 0.2, 0.2]
+post = []
+for i in range(len(sense)):
+    post.append(p[i] * sense[i])
+print post
