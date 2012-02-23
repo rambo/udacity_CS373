8c1bd5cb4334b3c3f7f51e5636ac48244698fbf0
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-23 23:51:29 +0200

    solutions to up to and including 9

--- unit1/unit1_video7_quiz.py
+++ unit1/unit1_video7_quiz.py
@@ -10,6 +10,16 @@ for i in range(0,world_size):
 print p
 sense = [0.2, 0.6, 0.6, 0.2, 0.2]
 post = []
+psum=0
 for i in range(len(sense)):
     post.append(p[i] * sense[i])
+    psum = psum + post[i]
 print post
+print psum
+norm = []
+nsum = 0
+for i in range(len(sense)):
+    norm.append(post[i] / psum)
+    nsum = nsum + norm[i]
+print norm
+print nsum
