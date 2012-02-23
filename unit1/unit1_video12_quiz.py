fdfee2cf78c00b720bcf372b16d79b2cb52e74df
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-24 00:04:09 +0200

    solution

--- unit1/unit1_video12_quiz.py
+++ unit1/unit1_video12_quiz.py
@@ -14,5 +14,8 @@ def sense(p, Z):
     for i in range(len(p)):
         hit = (Z == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    qsum = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / qsum
     return q
 print sense(p,Z)
