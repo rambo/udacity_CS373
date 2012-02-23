e513d0b8f3abd9b8d4063679f361020c7154013d
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-24 00:01:19 +0200

    next tasl

--- unit1/unit1_video12_quiz.py
+++ unit1/unit1_video12_quiz.py
@@ -1,8 +1,6 @@
-#Modify the code below so that the function sense, which 
-#takes p and Z as inputs, will output the NON-normalized 
-#probability distribution, q, after multiplying the entries 
-#in p by pHit or pMiss according to the color in the 
-#corresponding cell in world.
+#Modify your code so that it normalizes the output for 
+#the function sense. This means that the entries in q 
+#should sum to one.
 
 
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
@@ -12,9 +10,9 @@ pHit = 0.6
 pMiss = 0.2
 
 def sense(p, Z):
-    #
-    #ADD YOUR CODE HERE
-	#
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
     return q
-
 print sense(p,Z)
