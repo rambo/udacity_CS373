175aed08713db09afee019111e8e9461d31baf0e
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-25 22:21:15 +0200

    more helpers

--- unit1/homework_4.py
+++ unit1/homework_4.py
@@ -52,9 +52,63 @@ def normalize(p):
     return p
 #show(normalize(p))
 
+# Helper to do the multiplications on array
+def array_mul_2d(p, mul_by):
+    for row in range(len(p)):
+        # "in-place" (not actually, there will be temp lists created in memory) divide each item of the row by s
+        p[row][:] = [x * mul_by for x in p[row]]
+    return p
 
+def array_add_2d(p1, p2):
+    q = []
+    for row in range(len(p1)):
+        subq = []
+        for col in range(len(p1[row])):
+            subq.append(p1[row][col] + p2[row][col])
+        q.append(subq)
+    return q;
+#show(array_add_2d(p,p))
+
+def move_exact_2d(p, U):
+    # Special case
+    if (U == [0,0]):
+        return p 
+    
+    
+    return p
+
+def move_2d(p, U):
+    pass
+    
     
 
+
+# my odl list-slicing move
+#def move(p, U):
+#    U = U % len(p) * -1
+#    return p[U:] + p[:U]
+
+
+# old inexact motion examples
+#def move_exact(p, U):
+#    q = []
+#    for i in range(len(p)):
+#        q.append(p[(i-U)%len(p)])
+#    return q
+#
+#def sum_lists(p1, p2):
+#    q = []
+#    for i in range(len(p1)):
+#        q.append(p1[i] + p2[i])
+#    return q
+#
+#
+#def move(p, U):
+#    pE = [x * pExact for x in move_exact(p, U)]
+#    pO = [x * pOvershoot for x in move_exact(p, U+1)]
+#    pU = [x * pUndershoot for x in move_exact(p, U-1)]
+#    q = sum_lists(sum_lists(pE, pU), pO)
+#    return q
 
 
 
