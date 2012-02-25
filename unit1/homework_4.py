3ba8d9a759f7ae43a54b4f38029cb06b965a6da9
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-25 21:51:23 +0200

    p init and array sum helper

--- unit1/homework_4.py
+++ unit1/homework_4.py
@@ -24,6 +24,28 @@ def show(p):
 
 p = []
 
+# Assume (know) all rows have same length
+n_cells = len(colors) * len(colors[0])
+p_per_cell = 1.0 / n_cells
+print "%d cells, %f p of each cell" % (n_cells, p_per_cell)
+# initialize uniform distribution
+for i in range(len(colors)):
+    subp = []
+    for i in range(len(colors[0])):
+        subp.append(p_per_cell)
+    p.append(subp)
+
+# Helper to calculate the sum of 2d array
+def array_sum_2d(p):
+    tsum = 0 
+    for i in range(len(p)):
+        tsum = tsum + sum(p[i])
+    return tsum
+
+
+    
+print array_sum_2d(p)
+
 
 
 
