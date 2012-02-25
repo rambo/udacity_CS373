02da4061220db22c7f15675559a5063f9f0cd782
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-25 21:31:38 +0200

    adapted for the "house burns" homework

--- unit1/unit1_video35_quiz.py
+++ unit1/unit1_video35_quiz.py
@@ -1,8 +1,8 @@
 
-world = ['fair', 'not_fair']
-p = [0.5, 0.5]
+world = ['no_burn', 'burn']
+p = [0.999, 0.001]
 
-pHit = 0.5
+pHit = 0.9
 pMiss = 0.1
 
 
@@ -11,10 +11,11 @@ def sense(p, Z):
     for i in range(len(p)):
         hit = (Z == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    print q
     s = sum(q)
     for i in range(len(q)):
         q[i] = q[i] / s
     return q
 
 
-print sense(p, 'fair')
+print sense(p, 'burn')
