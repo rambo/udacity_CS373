0441ca8ffbd44bd19c5736717e8207d1f0e6a687
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-29 22:43:31 +0200

    calculating measurement updates

--- unit2/video18.py
+++ unit2/video18.py
@@ -1,8 +1,3 @@
-mu=10.0
-sigma2 = 4.0
-nu = 12.0
-r2 = 4
-
 
 def m_update(mu,sigma2,nu,r2):
     sigma2_p = 1.0/((1.0/sigma2)+(1.0/r2))
@@ -10,4 +5,4 @@ def m_update(mu,sigma2,nu,r2):
     return (mu_p, sigma2_p)
 
 
-print m_update(mu,sigma2,nu,r2)
+print m_update(1.0,2.0,25.0,2.0)
