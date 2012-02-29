9e445acb010ce6d7770697c381384117137d9e23
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-29 17:39:57 +0200

    the problem

---
+++ unit2/coconuts1.py
@@ -0,0 +1,5 @@
+# 5 guys, one monkey, pile of coconuts
+# one by one a guy takes 1/5 of the (remaining) pile, but since the pile
+# is not divisible by 5 gives one coconut to monkey.
+# After this the remaining pile is divided to five parts (giving one coconut to monkey)
+# monkey has now 6 coconuts, how many there were originally ?
