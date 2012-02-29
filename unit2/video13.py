e90a0c99e10f5e862765fbf82f3c4cc0b6470d6b
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-29 22:07:20 +0200

    used the loop to get to x=10.0 to maximize result

--- unit2/video13.py
+++ unit2/video13.py
@@ -4,7 +4,32 @@
 
 from math import *
 
+def frange(start, end=None, inc=None):
+    "A range function, that does accept float increments..."
+
+    if end == None:
+        end = start + 0.0
+        start = 0.0
+
+    if inc == None:
+        inc = 1.0
+
+    L = []
+    while 1:
+        next = start + len(L) * inc
+        if inc > 0 and next >= end:
+            break
+        elif inc < 0 and next <= end:
+            break
+        L.append(next)
+        
+    return L
+
+
 def f(mu, sigma2, x):
     return 1/sqrt(2.*pi*sigma2) * exp(-.5*(x-mu)**2 / sigma2)
 
-print f(10.,4.,8.) #Change the 8. to something else!
+mu = 10.0
+sigma2 = 4.0
+for fl in frange(7.0, 11.0, 0.1):
+    print "f(%f, %f, %f)=%f" % (mu, sigma2,fl,f(mu,sigma2,fl))
