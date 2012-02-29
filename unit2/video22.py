5b155f0f67a897877bda092b0c8231b79fbbc4fc
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-02-29 22:57:39 +0200

    movement updates

--- unit2/video22.py
+++ unit2/video22.py
@@ -9,4 +9,11 @@ def update(mean1, var1, mean2, var2):
     new_mean = ((var2*mean1)+(var1*mean2)) / (var2 + var1)
     return [new_mean, new_var]
 
+
+def move(mu,sigma2, u, r2):
+    new_mean = mu + u
+    new_var = sigma2 + r2
+    return [new_mean, new_var]
+
 print update(10.,8.,13., 2.)
+print move(8.0, 4.0, 10.0, 6.0)
