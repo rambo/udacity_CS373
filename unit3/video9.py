72b76fa26912a0e906beb251b22cfa1a512f9281
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-10 10:23:37 +0200

    solution

--- unit3/video9.py
+++ unit3/video9.py
@@ -116,3 +116,16 @@ def eval(r, p):
 
 myrobot = robot()
 
+# Make a robot called myrobot that starts at
+# coordinates 30, 50 heading north (pi/2).
+# Have your robot turn clockwise by pi/2, move
+# 15 m, and sense. Then have it turn clockwise
+# by pi/2 again, move 10 m, and sense again.
+
+myrobot.set(30., 50., pi/2)
+#print myrobot.sense()
+myrobot = myrobot.move(-1 * (pi/2), 15.)
+print myrobot.sense()
+myrobot = myrobot.move(-1 * (pi/2), 10.)
+print myrobot.sense()
+
