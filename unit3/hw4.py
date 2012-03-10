96d82db1f66934c80ad8ebd480dd975d6f14fc6b
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-10 14:13:01 +0200

    ready to submit

--- unit3/hw4.py
+++ unit3/hw4.py
@@ -142,23 +142,23 @@ class robot:
 ##       Robot:     [x=39.034 y=7.1270 orient=0.2886]
 ##
 ##
-length = 20.
-bearing_noise  = 0.0
-steering_noise = 0.0
-distance_noise = 0.0
-
-myrobot = robot(length)
-myrobot.set(0.0, 0.0, 0.0)
-myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
-
-motions = [[0.0, 10.0], [pi / 6.0, 10], [0.0, 20.0]]
-
-T = len(motions)
-
-print 'Robot:    ', myrobot
-for t in range(T):
-    myrobot = myrobot.move(motions[t])
-    print 'Robot:    ', myrobot
+##length = 20.
+##bearing_noise  = 0.0
+##steering_noise = 0.0
+##distance_noise = 0.0
+##
+##myrobot = robot(length)
+##myrobot.set(0.0, 0.0, 0.0)
+##myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
+##
+##motions = [[0.0, 10.0], [pi / 6.0, 10], [0.0, 20.0]]
+##
+##T = len(motions)
+##
+##print 'Robot:    ', myrobot
+##for t in range(T):
+##    myrobot = myrobot.move(motions[t])
+##    print 'Robot:    ', myrobot
 
 
 
