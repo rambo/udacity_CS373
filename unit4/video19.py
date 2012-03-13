00db5b63f94b298deb8de63638975aafc47958fa
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-13 23:05:27 +0200

    simple 2d (no orientation) a* from video12

--- unit4/video19.py
+++ unit4/video19.py
@@ -59,6 +59,55 @@ action_name = ['R', '#', 'L']
 # ----------------------------------------
 
 def optimum_policy2D():
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+
+
+    x = init[0]
+    y = init[1]
+    g = 0
+    f = heuristic[x][y]
+
+    open = [[f, g, x, y]]
+
+    found = False  # flag that is set when search is complet
+    resign = False # flag set if we can't find expand
+    count = 0
+    
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+        else:
+            open.sort()
+            next = open.pop(0) # we can pop the zeroeth element, no need to reverse
+#            open.reverse()
+#            next = open.pop()
+            x = next[2]
+            y = next[3]
+            g = next[1]
+            f = next[0]
+            expand[x][y] = count
+            count += 1
+            
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            f2 = g2 + heuristic[x2][y2]
+                            open.append([f2, g2, x2, y2])
+                            closed[x2][y2] = 1
+
+    for i in range(len(expand)):
+        print expand[i]
+
+
 
     return policy2D # Make sure your function returns the expected grid.
 
