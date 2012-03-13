3c28ca3a8ff1badf5092eaf81c3501761ea55c28
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-13 22:02:52 +0200

    solution

--- unit4/video17.py
+++ unit4/video17.py
@@ -34,5 +34,49 @@ cost_step = 1 # the cost associated with moving from a cell to an adjacent one.
 # ----------------------------------------
 
 def compute_value():
+    values = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    x = goal[0]
+    y = goal[1]
+    closed[x][y] = 1
+    f = 0
+    open = [[f, x, y]]
+    while (True):
+        if len(open) == 0:
+            break
+        
+        #open.sort() # not needed for this algo, we evaluate every cell
+        next = open.pop(0) # we can pop the zeroeth element, no need to reverse
+        x = next[1]
+        y = next[2]
+        f = next[0]
+        closed[x][y] = 1
+        values[x][y] = f
 
-    return value #make sure your function returns a grid of values as demonstrated in the previous video.
+        for i in range(len(delta)):
+            f2 = f + cost_step
+            x2 = x + delta[i][0]
+            y2 = y + delta[i][1]
+            if (   x2 < 0 # Skip values outside of the grid
+                or y2 < 0
+                or x2 > len(grid)-1
+                or y2 > len(grid[0])-1):
+                #print "(%d,%d) is outside the grid" % (x2, y2)
+                continue
+            if (closed[x2][y2] == 1): #Already checked
+                #print "(%d,%d) is already checked" % (x2, y2)
+                continue
+            if (grid[x2][y2] == 1): #Occupied space, do not expand
+                #print "(%d,%d) is occupied" % (x2, y2)
+                continue
+            open.append([f2,x2,y2])
+
+    
+    
+
+    for i in range(len(values)):
+        print values[i]
+    return values #make sure your function returns a grid of values as demonstrated in the previous video.
+
+
+compute_value()
