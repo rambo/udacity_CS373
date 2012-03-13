6f6a6c255567d32248c68900d9262bf095eccceb
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-13 20:32:13 +0200

    modified the video9 base (which is essentially the preferred solution to video8?) to get "you got it right" but I still think my original approach was fine

--- unit4/video8_inlined.py
+++ unit4/video8_inlined.py
@@ -34,74 +34,50 @@ delta_name = ['^', '<', 'v', '>']
 
 cost = 1
 
+
+# ----------------------------------------
+# modify code below
+# ----------------------------------------
+
 def search():
-    # checked = [[0] * len(grid[0])] * len(grid) # arghs, this will create references...
-    checked = []
-    for i in range(len(grid)):
-        checked.append([0] * len(grid[0]))
-
-    expand_table = []
-    for i in range(len(grid)):
-        expand_table.append([-1] * len(grid[0]))
-        
-    expand_counter = 0
-
-    def show_array(a):
-        for i in range(len(a)):
-            print a[i]
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
 
     x = init[0]
     y = init[1]
-    g = 0 # cost
-    
-    to_check = [[g,x,y]] # Sebastian calls this open but that's a reserved word
-    
-    while(True):
-        if len(to_check) == 0:
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
             print 'fail'
             return 'fail'
-        
-        to_check.sort()
-        next_check = to_check.pop(0) # Sebastian calls this next but it too is a reserved word
-        x = next_check[1]
-        y = next_check[2]
-        g = next_check[0]
-        
-        if (    x == goal[0]
-            and y == goal[1]):
-            #print "solution"
-            print next_check
-            return next_check
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            
+            if x == goal[0] and y == goal[1]:
+                found = True
+                print next
+                return next
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
 
-        checked[x][y] = 1
 
-        new_cost = g + cost
-        for i in range(len(delta)):
-            new_x = x + delta[i][0]
-            new_y = y + delta[i][1]
-            if (   new_x < 0 # Skip values outside of the grid
-                or new_y < 0
-                or new_x > len(grid)-1
-                or new_y > len(grid[0])-1):
-                #print "(%d,%d) is outside the grid" % (new_x, new_y)
-                continue
-            if (grid[new_x][new_y] == 1): #Occupied space, we cannot expand here
-                #print "(%d,%d) is occupied" % (new_x, new_y)
-                checked[new_x][new_y] = 1 # Mark it as checked while at it
-                continue
-            if (checked[new_x][new_y]): # Already checked, do not expand here
-                #print "(%d,%d) is already checked" % (new_x, new_y)
-                continue
-            to_check.append([new_cost,new_x,new_y])
-            expand_table[new_x][new_y] = expand_counter
-            expand_counter += 1
-        
-#        print "Checked"
-#        show_array(checked)
-#        print "To check"
-#        show_array(to_check)
-
-search()        
-        
-        
-    
+search()
