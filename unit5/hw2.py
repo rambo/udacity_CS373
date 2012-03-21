64b445b9d394b1cd66cacf39848d51199686cfe0
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-21 21:29:00 +0200

    cyclic implementation based on the open path example from video5

--- unit5/hw2.py
+++ unit5/hw2.py
@@ -58,10 +58,49 @@ path=[[0, 0],
 
 def smooth(path, weight_data = 0.1, weight_smooth = 0.1, tolerance = 0.00001):
 
-    # 
-    # Enter code here
-    #
-    
+    # Make a deep copy of path into newpath
+    newpath = [[0 for row in range(len(path[0]))] for col in range(len(path))]
+    for i in range(len(path)):
+        for j in range(len(path[0])):
+            newpath[i][j] = path[i][j]
+
+
+    #### ENTER CODE BELOW THIS LINE ###
+    alpha = weight_data
+    beta = weight_smooth
+    # Seems these tolerances are way too high...
+    #tolerance = 0.001 * (len(path)-2)
+    tmppath = [[0 for row in range(len(path[0]))] for col in range(len(path))]
+    last_total_change = 0.0
+    while True:
+        total_change = 0.0
+        for i in range(len(path)):
+            for j in range(len(path[i])): # Update each term (we don't have the matrix class now...)
+                # "official example (that in actually a bit wrong, see http://www.udacity-forums.com/cs373/questions/23361/unit5-5-gradient-descent-simultaneous-update)
+                tmptmp = newpath[i][j]
+                newpath[i][j] += weight_data * (path[i][j] - newpath[i][j])
+                newpath[i][j] += weight_smooth * (newpath[(i-1)%len(newpath)][j] + newpath[(i+1)%len(newpath)][j] - (2.0 * newpath[i][j]))
+                total_change += abs(tmptmp -  newpath[i][j])
+                
+                
+#                # Version doing it properly (from same URL)
+#                alpha_value = alpha * (path[i][j] - newpath[i][j])
+#                beta_value = beta * (newpath[(i+1)%len(newpath)][j] + newpath[(i-1)%len(newpath)][j] - 2 * newpath[i][j])
+#                tmppath[i][j] += alpha_value + beta_value
+#                
+#                #print "alpha_value=%f beta_value=%f" % (alpha_value, beta_value)
+#
+#                total_change += abs(alpha_value+beta_value) # This produces a value approaching zero
+            
+        newpath = tmppath
+
+        #print "total_change %f (tolerance %f)" % (total_change, tolerance)
+        if (   total_change < tolerance # Good enough
+            or total_change == last_total_change): # Stops changing
+            break
+        
+        last_total_change = total_change
+
     return newpath
 
 # thank you - EnTerr - for posting this on our discussion forum
@@ -214,7 +253,10 @@ answer2 = [[1.239080543767428, 0.5047204351187283],
 ##    [0.000, 1.000] -> [0.505, 1.239]
 ##    [1.000, 1.000] -> [0.908, 0.908]
 
-# solution_check(smooth(testpath1), answer1)
+print "Test path 1"
+solution_check(smooth(testpath1), answer1)
+print "Test path 2"
+solution_check(smooth(testpath2), answer2)
 
 
 
