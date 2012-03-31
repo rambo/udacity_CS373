57f6e0061aa0a972493f96e62980f61e248eae76
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-03-31 13:59:24 +0300

    copied twiddle forgotten to the previous example but it is sloooow, did not optimize, just adjusted the params a bit by hand

--- unit6/video7.py
+++ unit6/video7.py
@@ -525,8 +525,8 @@ def run(grid, goal, spath, params, printflag = False, speed = 0.1, timeout = 100
         Z = myrobot.sense()
         filter.sense(Z)
 
-        if not myrobot.check_collision(grid):
-            print '##### Collision ####'
+        #if not myrobot.check_collision(grid):
+        #    print '##### Collision ####'
 
         err += (cte ** 2)
         N += 1
@@ -585,11 +585,86 @@ weight_smooth     = 0.2
 p_gain            = 2.0
 d_gain            = 6.0
 
+
+weight_dataweight_data       = 0.1
+weight_smooth     = 0.2
+p_gain            = 1.8
+d_gain            = 5.9
+
 ###################################
-    
+
+#for i in range(10):
+#    print i, ':', main(grid, init, goal, steering_noise, distance_noise, measurement_noise, 
+#               weight_data, weight_smooth, p_gain, d_gain)
+
 print main(grid, init, goal, steering_noise, distance_noise, measurement_noise, 
-           weight_data, weight_smooth, p_gain, d_gain)
+            weight_data, weight_smooth, p_gain, d_gain)
 
 
+def twiddle(init_params):
+    n_params   = len(init_params)
+    dparams    = [1.0 for row in range(n_params)]
+    params     = [0.0 for row in range(n_params)]
+    K = 10
+
+    for i in range(n_params):
+        params[i] = init_params[i]
+
+
+    best_error = 0.0;
+    for k in range(K):
+        ret = main(grid, init, goal, 
+                   steering_noise, distance_noise, measurement_noise, 
+                   params[0], params[1], params[2], params[3])
+        if ret[0]:
+            best_error += ret[1] * 100 + ret[2]
+        else:
+            best_error += 99999
+    best_error = float(best_error) / float(k+1)
+    print best_error
+
+    n = 0
+    #while sum(dparams) > 0.0000001:
+    while sum(dparams) > 0.00001:
+        for i in range(len(params)):
+            params[i] += dparams[i]
+            err = 0
+            for k in range(K):
+                ret = main(grid, init, goal, 
+                           steering_noise, distance_noise, measurement_noise, 
+                           params[0], params[1], params[2], params[3])
+                if ret[0]:
+                    err += ret[1] * 100 + ret[2]
+                else:
+                    err += 99999
+            print float(err) / float(k+1)
+            if err < best_error:
+                best_error = float(err) / float(k+1)
+                dparams[i] *= 1.1
+            else:
+                params[i] -= 2.0 * dparams[i]            
+                err = 0
+                for k in range(K):
+                    ret = main(grid, init, goal, 
+                               steering_noise, distance_noise, measurement_noise, 
+                               params[0], params[1], params[2], params[3])
+                    if ret[0]:
+                        err += ret[1] * 100 + ret[2]
+                    else:
+                        err += 99999
+                print float(err) / float(k+1)
+                if err < best_error:
+                    best_error = float(err) / float(k+1)
+                    dparams[i] *= 1.1
+                else:
+                    params[i] += dparams[i]
+                    dparams[i] *= 0.5
+        n += 1
+        print 'Twiddle #', n, params, ' -> ', best_error
+    print ' '
+    return params
+
+
+#print twiddle([weight_data, weight_smooth, p_gain, d_gain])
 
 
