09cd69a4d17e6491662f60c8b3207e2f116f9b0e
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-04-01 11:01:56 +0300

    trying out the take() method

--- unit6/matrix_test.py
+++ unit6/matrix_test.py
@@ -21,8 +21,21 @@ keep_indices += range(first_lm_idx+2,new_dimx)
 
 print "keep_indices=%s" % repr(keep_indices)
 
-m1 = m1.expand(new_dimx, new_dimx, keep_indices, keep_indices)
+m2 = m1.expand(new_dimx, new_dimx, keep_indices, keep_indices)
 
 
-print "m1 expanded"
-m1.show()
+print "m1 expanded (m2)"
+m2.show()
+
+
+m3 = m1.take([0,1],range(m1.dimx-(num_landmarks*2), m1.dimx))
+print "m1 slice A (m3)"
+m3.show()
+
+m4 = m1.take([0,1],[0,1])
+print "m1 slice B (m4)"
+m4.show()
+
+m5 = m1.take(range(2,m1.dimx))
+print "m1 slice O1 (m5)"
+m5.show()
